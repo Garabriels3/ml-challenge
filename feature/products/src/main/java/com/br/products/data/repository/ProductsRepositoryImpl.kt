db49718f971073bc4d52f5c7737d8f348f001b39
Gabriel dos Santos Nascimento <60590850+Garabriels3@users.noreply.github.com>
2024-04-24 19:52:49 -0300

    create repository to pager products putting pager limit

---
+++ feature/products/src/main/java/com/br/products/data/repository/ProductsRepositoryImpl.kt
@@ -0,0 +1,61 @@
+package com.br.products.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.br.network.errorutils.parseHttpError
+import com.br.products.data.datasource.remote.ProductPagingSource
+import com.br.products.data.model.response.ProductItemResponse
+import com.br.products.data.model.response.ShippingResponse
+import com.br.products.data.service.ProductsService
+import com.br.products.domain.model.ProductItemDomain
+import com.br.products.domain.model.ShippingDomain
+import com.br.products.domain.repository.ProductsRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+
+private const val NETWORK_PAGE_SIZE = 20
+
+class ProductsRepositoryImpl(
+    private val service: ProductsService,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ProductsRepository {
+
+    override fun getProductsPagingData(query: String): Flow<PagingData<ProductItemDomain>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = NETWORK_PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { ProductPagingSource(service, query) }
+        ).flow.map { page ->
+            page.map {
+                it.toDomain()
+            }
+        }
+            .flowOn(dispatcher)
+            .parseHttpError()
+    }
+
+    private fun ProductItemResponse.toDomain(): ProductItemDomain {
+        return ProductItemDomain(
+            id = id,
+            title = title,
+            price = price,
+            thumbnail = thumbnail,
+            condition = condition,
+            availableQuantity = availableQuantity,
+            shipping = shipping.toDomain()
+        )
+    }
+
+    private fun ShippingResponse.toDomain(): ShippingDomain {
+        return ShippingDomain(
+            freeShipping = freeShipping
+        )
+    }
+}
---
+++ feature/products/src/main/java/com/br/products/domain/repository/ProductsRepository.kt
@@ -0,0 +1,9 @@
+package com.br.products.domain.repository
+
+import androidx.paging.PagingData
+import com.br.products.domain.model.ProductItemDomain
+import kotlinx.coroutines.flow.Flow
+
+interface ProductsRepository {
+    fun getProductsPagingData(query: String): Flow<PagingData<ProductItemDomain>>
+}
