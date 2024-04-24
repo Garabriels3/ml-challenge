174ac95fd766d605fa0898c56da45508dbf11210
Gabriel dos Santos Nascimento <60590850+Garabriels3@users.noreply.github.com>
2024-04-24 16:34:30 -0300

    create network module

---
+++ network/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrainsKotlinJvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.adapter.corutines)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.moshi)
+    implementation(libs.retrofit.converter.kotlin.serialization)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "ML Challenge"
 include(":app")
 include(":design_system")
+include(":network")
