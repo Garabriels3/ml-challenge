34980275a2b9e5b065795563416b43dabaa64b78
Gabriel dos Santos Nascimento <60590850+Garabriels3@users.noreply.github.com>
2024-04-24 19:51:51 -0300

    create paging logic to consume list

---
+++ feature/products/src/main/java/com/br/products/data/datasource/remote/ProductPagingSource.kt
@@ -0,0 +1,31 @@
+package com.br.products.data.datasource.remote
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.br.products.data.service.ProductsService
+import com.br.products.data.model.response.ProductItemResponse
+
+class ProductPagingSource(
+    private val service: ProductsService,
+    private val query: String
+) : PagingSource<Int, ProductItemResponse>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ProductItemResponse> {
+        return try {
+            val nextPage = params.key ?: 0
+            val response = service.getProducts(query, params.loadSize, nextPage)
+
+            LoadResult.Page(
+                data = response.productsItem.map { it.copy(total = response.paging.total) },
+                prevKey = if (nextPage == 0) null else nextPage - params.loadSize,
+                nextKey = if (response.productsItem.isEmpty()) null else nextPage + params.loadSize
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, ProductItemResponse>): Int? {
+        return state.anchorPosition
+    }
+}
