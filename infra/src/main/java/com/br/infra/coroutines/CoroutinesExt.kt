2a04dfbea5658b12896e13ec9cdd799abfcab858
Gabriel dos Santos Nascimento <60590850+Garabriels3@users.noreply.github.com>
2024-04-25 15:49:06 -0300

    fix theme problems of not find material 3 imports

--- infra/src/main/java/com/br/infra/coroutines/CoroutinesExt.kt
+++ infra/src/main/java/com/br/infra/coroutines/CoroutinesExt.kt
@@ -5,14 +5,14 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Observer
 
-@Composable
-fun <T> SingleLiveEvent<T>.collectEffect(block: (T) -> Unit) {
-    val lifecycle = LocalLifecycleOwner.current
-    DisposableEffect(key1 = this) {
-        val observer = Observer<T> { block(it) }
-        this@collectEffect.observe(lifecycle, observer)
-        onDispose {
-            this@collectEffect.removeObserver(observer)
-        }
-    }
-}
+//@Composable
+//fun <T> SingleLiveEvent<T>.collectEffect(block: (T) -> Unit) {
+//    val lifecycle = LocalLifecycleOwner.current
+//    DisposableEffect(key1 = this) {
+//        val observer = Observer<T> { block(it) }
+//        this@collectEffect.observe(lifecycle, observer)
+//        onDispose {
+//            this@collectEffect.removeObserver(observer)
+//        }
+//    }
+//}
