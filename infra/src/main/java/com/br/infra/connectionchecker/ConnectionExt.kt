f5c81a805fcd8243a84dc355434f0250fdbae9f5
Gabriel dos Santos Nascimento <60590850+Garabriels3@users.noreply.github.com>
2024-04-25 14:21:41 -0300

    create a extension to verify internet capabilities and validating connection. Add a permission of internet checker

---
+++ infra/src/main/java/com/br/infra/connectionchecker/ConnectionExt.kt
@@ -0,0 +1,13 @@
+package com.br.infra.connectionchecker
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun Context.isNetworkAvailable(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val network = connectivityManager.activeNetwork ?: return false
+    val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+    return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+           capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+}
